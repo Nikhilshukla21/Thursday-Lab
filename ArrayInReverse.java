efe0379d10ee3dc98a0e81706e9b101f40cbc40a
Nikhilshukla21 <106609510+Nikhilshukla21@users.noreply.github.com>
2022-07-01 13:21:22 +0530

    Add files via upload

---
+++ ArrayInReverse.java
@@ -0,0 +1,38 @@
+//Program to print the elements of an array in reverse order
+package arrayexamples;
+
+import java.util.Scanner;
+
+public class ArrayInReverse {
+
+	public static void main(String[] args) {
+		int size, i, arr[];// declare variable
+		Scanner sc = new Scanner(System.in);
+
+		// receive size of an array from user
+		System.out.println("Enter size of an array");
+		size = sc.nextInt();
+
+		// instantiate array
+		arr = new int[size];
+
+		// getting array elements from user
+		System.out.println("Enter " + size + " elements in an array");
+		for (i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.println("Given array is");
+		// for printing given array
+		for (i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println("Array after reverse");
+		// for printing reverse of array
+		for (i = arr.length - 1; i >= 0; i--) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
